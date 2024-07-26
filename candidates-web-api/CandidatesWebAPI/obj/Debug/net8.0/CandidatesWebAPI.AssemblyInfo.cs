2262e37240d68bece568a813d36d9e9b32f2bbcd
pauljtom <paulthomson737@gmail.com>
2024-07-26 22:08:46 +0200

    --main fix

--- candidates-web-api/CandidatesWebAPI/obj/Debug/net8.0/CandidatesWebAPI.AssemblyInfo.cs
+++ candidates-web-api/CandidatesWebAPI/obj/Debug/net8.0/CandidatesWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CandidatesWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b736680df369a4bab218d366b1c18009d9ff1dd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CandidatesWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CandidatesWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
